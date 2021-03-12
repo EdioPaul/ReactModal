a1fa2a072b1b8320f5336aa2b52421f12b0c06c3
EdioPaul <ediopaulo.ep@gmail.com>
2021-03-12 14:25:11 -0300

    Criando um componente de Modal no ReactJS

---
+++ modal/src/App.js
@@ -0,0 +1,24 @@
+import React, {useState} from 'react';
+import './App.css';
+
+import Modal from './components/Modal';
+
+function App() {
+  const [isModalVisible, setIsModalVisible] = useState(false);
+  return (
+    <div className="App">
+      <button onClick={() => setIsModalVisible(true)}> Open </button>
+      {isModalVisible ? (
+      <Modal onClose={() => setIsModalVisible(false)}>
+        <h2>Modal do App</h2>
+        <p>
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+        </p>
+      </Modal>
+       ): null}
+    </div>
+  );
+}
+
+
+export default App;
